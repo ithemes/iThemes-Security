f225d7cd7b9b43cfe662a328ddac9b45f846e76e
Chris Wiegman <chris@bit51.com>
2013-06-02 11:45:12 -0500

    Framework for 4.0

--- better-wp-security.php
+++ better-wp-security.php
@@ -2,178 +2,118 @@
 /*
 	Plugin Name: Better WP Security
 	Plugin URI: http://bit51.com/software/better-wp-security/
-	Description: Helps protect your Wordpress installation from attackers. Hardens standard Wordpress security by hiding vital areas of your site, protecting access to important files via htaccess, preventing brute-force login attempts, detecting attack attempts, and more.
-	Version: 3.5.2
-	Text Domain: better-wp-security
+	Description: Protect your WordPress site by hiding vital areas of your site, protecting access to important files, preventing brute-force login attempts, detecting attack attempts and more.
+	Version: Dev
+	Text Domain: better_wp_security
 	Domain Path: /languages
 	Author: Bit51
 	Author URI: http://bit51.com
 	License: GPLv2
-	Copyright 2012 Bit51.com  (email : info@bit51.com)
+	Copyright 2011-2013  Bit51  (email : info@bit51.com)
 */
 
-//Require common Bit51 library
-require_once( plugin_dir_path( __FILE__ ) . 'lib/bit51/bit51.php' );
+if ( ! class_exists( 'Bit51_BWPS' ) ) {
 
-if ( ! class_exists( 'bit51_bwps' ) ) {
 
-	class bit51_bwps extends Bit51Foo {
-	
-		public $pluginversion 	= '3062'; //current plugin version
-	
-		//important plugin information
-		public $hook 				= 'better-wp-security';
-		public $pluginbase			= 'better-wp-security/better-wp-security.php';
-		public $pluginname			= 'Better WP Security';
-		public $homepage			= 'http://bit51.com/software/better-wp-security/';
-		public $supportpage 		= 'http://wordpress.org/support/plugin/better-wp-security';
-		public $wppage 				= 'http://wordpress.org/extend/plugins/better-wp-security/';
-		public $accesslvl			= 'manage_options';
-		public $paypalcode			= 'V647NGJSBC882';
-		public $plugindata 			= 'bit51_bwps_data';
-		public $primarysettings		= 'bit51_bwps';
-		public $settings			= array(
-			'bit51_bwps_options'	=> array(
-				'bit51_bwps' 				=> array(
-					'initial_backup'			=> '0',
-					'initial_filewrite'			=> '0',
-					'am_enabled'				=> '0',
-					'am_type' 					=> '0',
-					'am_startdate' 				=> '1',
-					'am_enddate' 				=> '1',
-					'am_starttime' 				=> '1',
-					'am_endtime' 				=> '1',
-					'backup_email' 				=> '1',
-					'backup_emailaddress' 		=> '',
-					'backup_time'				=> '1',
-					'backup_interval'			=> '1',
-					'backup_enabled'			=> '0',
-					'backup_last'				=> '',
-					'backup_next'				=> '',
-					'backups_to_retain'			=> '10',
-					'bu_enabled' 				=> '0',
-					'bu_banlist' 				=> '',
-					'bu_banagent'				=> '',
-					'bu_blacklist'				=> '0',
-					'hb_enabled'				=> '0',
-					'hb_login'					=> 'login',
-					'hb_register'				=> 'register',
-					'hb_admin'					=> 'admin',
-					'hb_key'					=> '',
-					'll_enabled' 				=> '0',
-					'll_maxattemptshost' 		=> '5',
-					'll_maxattemptsuser' 		=> '10',
-					'll_checkinterval' 			=> '5',
-					'll_banperiod' 				=> '15',
-					'll_blacklistip'			=> '1',
-					'll_blacklistipthreshold'	=> '3',
-					'll_emailnotify' 			=> '1',
-					'll_emailaddress'			=> '',
-					'id_enabled' 				=> '0',
-					'id_emailnotify' 			=> '1',
-					'id_checkinterval' 			=> '5',
-					'id_threshold' 				=> '20',
-					'id_banperiod' 				=> '15',
-					'id_blacklistip'			=> '0',
-					'id_blacklistipthreshold'	=> '3',
-					'id_whitelist' 				=> '',
-					'id_emailaddress'			=> '',
-					'id_fileenabled'			=> '0',
-					'id_fileemailnotify'		=> '1',
-					'id_filedisplayerror'		=> '1',
-					'id_fileemailaddress'		=> '',
-					'id_specialfile'			=> '',
-					'id_fileincex'				=> '1',
-					'id_filechecktime'			=> '',
-					'st_ht_files'				=> '0',
-					'st_ht_browsing'			=> '0',
-					'st_ht_request'				=> '0',
-					'st_ht_query'				=> '0',
-					'st_generator'				=> '0',
-					'st_manifest'				=> '0',
-					'st_edituri'				=> '0',
-					'st_themenot'				=> '0',
-					'st_pluginnot'				=> '0',
-					'st_corenot'				=> '0',
-					'st_enablepassword'			=> '0',
-					'st_passrole'				=> 'administrator',
-					'st_loginerror'				=> '0',
-					'st_fileperm'				=> '0',
-					'st_comment'				=> '0',
-					'st_randomversion'			=> '0',
-					'st_longurl'				=> '0',
-					'st_fileedit'				=> '0',
-					'st_writefiles'				=> '0',
-					'ssl_forcelogin'			=> '0',
-					'ssl_forceadmin'			=> '0',
-					'ssl_frontend'				=> '0',
-					'oneclickchosen'			=> '0'
-				)
-			)
-		);
-		public $tabs;
+	/**
+	 * Plugin class used to create plugin object and load both core and needed modules
+	 */
+	final class Bit51_BWPS {
 
-		function __construct() {
-		
-			global $bwps, $bwpsoptions, $bwpsdata;
-			
-			//Get the options
-			if ( is_multisite() ) {
-			
-				switch_to_blog( 1 );
-			
-				$bwpsoptions = get_option( $this->primarysettings );
-				$bwpsdata = get_option( $this->plugindata );
-			
-				restore_current_blog();
-			
-			} else {
-			
-				$bwpsoptions = get_option( $this->primarysettings );
-				$bwpsdata = get_option( $this->plugindata );
-				
-			}
-		
-			//set path information
-			
-			if ( ! defined( 'BWPS_PP' ) ) {
-				define( 'BWPS_PP', plugin_dir_path( __FILE__ ) );
-			}
-			
-			if ( ! defined( 'BWPS_PU' ) ) {
-				define( 'BWPS_PU', plugin_dir_url( $this->pluginbase, __FILE__ ) );
-			}
-		
-			//load the text domain
-			load_plugin_textdomain( 'better-wp-security', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-		
-			//require admin pages
-			if ( is_admin() || ( is_multisite() && is_network_admin() ) ) {
-				require_once( BWPS_PP . 'inc/admin/construct.php' );
-			}
-			
-			//require setup information
-			require_once( BWPS_PP . 'inc/setup.php' );
-			register_activation_hook( __FILE__, array( 'bwps_setup', 'on_activate' ) );
-			register_deactivation_hook( __FILE__, array( 'bwps_setup', 'on_deactivate' ) );
-			register_uninstall_hook( __FILE__, array( 'bwps_setup', 'on_uninstall' ) );
-			
-			require_once( BWPS_PP . 'inc/auth.php' );
-			require_once( BWPS_PP . 'inc/secure.php' );
-			$bwps = new bwps_secure();
-		
-			if ( $bwpsdata['version'] != $this->pluginversion || get_option( 'BWPS_options' ) != false ) {
-				new bwps_setup( 'activate', true );
-			}
+		private static $instance = null; //instantiated instance of this plugin
 
-			parent::init();
+		public //see documentation upon instantiation 
+			$core,
+			$dashboard_menu_title,
+			$dashboard_page_name,
+			$globals,
+			$menu_icon,
+			$menu_name,
+			$settings_menu_title,
+			$settings_page,
+			$settings_page_name,
+			$top_level_menu;
+
+		/**
+		 * Default plugin execution used for settings defaults and loading components
+		 * 
+		 * @return void
+		 */
+		private function __construct() {
+
+			//Set plugin defaults
+			$this->globals = array(
+				'plugin_build'			=> 3062, //plugin build number - used to trigger updates
+				'plugin_file'			=> __FILE__, //the main plugin file
+				'plugin_access_lvl' 	=> 'manage_options', //Access level required to access plugin options
+				'plugin_dir' 			=> plugin_dir_path( __FILE__ ), //the path of the plugin directory
+				'plugin_homepage' 		=> 'http://bit51.com/software/better-wp-security/', //The plugins homepage on WordPress.org
+				'plugin_hook'			=> 'better_wp_security', //the hook for text calls and other areas
+				'plugin_name' 			=> __( 'Better WP Security', 'better_wp_security' ), //the name of the plugin
+				'plugin_url' 			=> plugin_dir_url( __FILE__ ), //the URL of the plugin directory
+				'support_page' 			=> 'http://wordpress.org/support/plugin/better-wp-security', //address of the WordPress support forums for the plugin
+				'wordpress_page'		=> 'http://wordpress.org/extend/plugins/better-wp-security/', //plugin's page in the WordPress.org Repos
+			);
+
+			$this->top_level_menu = true; //true if top level menu, else false
+			$this->menu_name = __( 'Security', $this->globals['plugin_hook'] ); //main menu item name
+
+			//the following options must only be set if it's a top-level section
+			$this->settings_page = true; //when using top_level menus this will always create a "Dashboard" page. Should it create a settings page as well?
+			$this->menu_icon = $this->globals['plugin_url'] . 'img/shield-small.png'; //image icon 
+			$this->dashboard_menu_title = __( 'Dashboard', $this->globals['plugin_hook'] ); //the name of the dashboard menu item (if different "Dashboard")
+			$this->settings_menu_title = __( 'Settings', $this->globals['plugin_hook'] ); //the name of the settings menu item (if different from "Settings")
+			$this->dashboard_page_name = __( 'Dashboard', $this->globals['plugin_hook'] ); //page name - appears after plugin name on the dashboard page
+			$this->settings_page_name = __( 'Options', $this->globals['plugin_hook'] ); //page name - appears after plugin name on the dashboard page
+			
+
+			//load core functionality for admin use
+			require_once( $this->globals['plugin_dir'] . 'inc/class-bit51-bwps-core.php' );
+			$this->core = Bit51_BWPS_Core::start( $this );
+
+			//load modules
+			$this->load_modules();
+
+			//builds admin menus after modules are loaded
+			if ( is_admin() ) {
+				$this->core->build_admin(); 
+			}
+			
 		}
-		
+
+		/**
+		 * Loads required plugin modules
+		 *
+		 * Note: Do not modify this area other than to specify modules to load. 
+		 * Build all functionality into the appropriate module.
+		 * 
+		 * @return void
+		 */
+		public function load_modules() {
+
+			//load BWPS Dashboard module
+			require_once( $this->globals['plugin_dir'] . 'modules/bit51-bwps-dashboard/class-bit51-bwps-dashboard.php' );
+			Bit51_BWPS_Dashboard::start( $this->core );
+			
+		}
+
+		/**
+		 * Start the plugin
+		 * 
+		 * @return Bit51_BWPS     The instance of the plugin
+		 */
+		public static function start() {
+
+			if ( ! isset( self::$instance ) || self::$instance === null ) {
+				self::$instance = new self;
+			}
+
+			return self::$instance;
+
+		}
+
 	}
-	
+
 }
 
-//create plugin object
-global $bwpsobject;
-$bwpsobject = new bit51_bwps();
-
+Bit51_BWPS::start();
---
+++ inc/class-bit51-bwps-setup.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * Dispatches seup processes from individual modules
+ * 
+ * @version 1.0
+ */
+
+if ( ! class_exists( 'Bit51_BWPS_Setup' ) ) {
+
+	class Bit51_BWPS_Setup {
+
+		private
+			$hook,
+			$update;
+
+		/**
+		 * Establish setup object
+		 *
+		 * Establishes set object and calls appropriate execution function
+		 *
+		 * @param bool $case[optional] Appropriate execution module to call
+		 *
+		 **/
+		function __construct( $case = false, $upgrading = false ) {
+
+			//Important, this must be manually set in each plugin
+			$this->hook = 'better_wp_security';
+	
+			if ( ! $case ) {
+				die( 'error' );
+			}
+
+			switch($case) {
+				case 'activate': //active plugin
+					$this->activate_execute( $upgrading );
+					break;
+
+				case 'deactivate': //deactivate plugin
+					$this->deactivate_execute();
+					break;
+
+				case 'uninstall': //uninstall plugin
+					$this->uninstall_execute();
+					break;
+			}
+
+		}
+
+		/**
+		 * Execute setup script for each module installed
+		 * 
+		 * @return void
+		 */
+		function do_modules() {
+
+			$modules_folder = dirname(__FILE__) . '/../modules';
+
+			$modules = scandir( $modules_folder );
+			
+			foreach ( $modules as $module ) {
+				
+				$module_folder = $modules_folder . '/' . $module;
+
+				if ( $module !== '.' && $module !== '..' && is_dir( $module_folder ) && file_exists( $module_folder . '/setup.php' ) ) {
+
+					require_once( $module_folder . '/setup.php' );
+
+				}
+
+			}
+
+		}
+		
+		/**
+		 * Public function to activate
+		 *
+		 **/
+		function on_activate() {
+			
+			define( 'BWPS_NEW_INSTALL', true );
+			new Bit51_BWPS_Setup( 'activate' );
+			
+		}
+
+		/**
+		 * Public function to deactivate
+		 *
+		 **/
+		function on_deactivate() {
+	
+			$devel = true; //set to true to uninstall for development
+		
+			if ( $devel ) {
+				$case = 'uninstall';
+			} else {
+				$case = 'deactivate';
+			}
+
+			new Bit51_BWPS_Setup( $case );
+		}
+
+		/**
+		 * Public function to uninstall
+		 *
+		 **/
+		function on_uninstall() {
+		
+			new Bit51_BWPS_Setup( 'uninstall' );
+			
+		}
+		
+		/**
+		 * Execute activation
+		 * 
+		 * @param  boolean $updating true if the plugin is updating
+		 * @return void
+		 */
+		function activate_execute( $updating = false ) {
+			global $bwps_setup_action;
+
+			$bwps_setup_action = 'activate';
+
+			$this->do_modules();
+
+			do_action( $this->hook . '_set_plugin_data' );
+			
+		}
+
+		/**
+		 * Update Execution
+		 * 
+		 * @param  string $oldversion Old version number
+		 * @return void
+		 */
+		function upgrade_execute( $oldversion = '' ) {
+			global $bwps_setup_action;
+
+			$bwps_setup_action = 'upgrade';
+
+			$this->do_modules();
+			
+		}
+		
+		/**
+		 * Deactivate execution
+		 *
+		 **/
+		function deactivate_execute( $updating = false ) {
+			global $bwps_setup_action;
+
+			$bwps_setup_action = 'deactivate';
+
+			$this->do_modules();
+			
+		}
+		
+		/**
+		 * Uninstall execution
+		 *
+		 **/
+		function uninstall_execute() {
+			global $bwps_setup_action;
+
+			$bwps_setup_action = 'uninstall';
+
+			$this->do_modules();
+
+			delete_option( $this->hook . '_data' );
+			
+			if ( function_exists( 'apc_store' ) ) { 
+				apc_clear_cache(); //Let's clear APC (if it exists) when big stuff is saved.
+			}
+			
+		}
+		
+	}
+	
+}
