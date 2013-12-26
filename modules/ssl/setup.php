3217273e3cb090d978d99782f01b984dd6ee6d22
Chris Wiegman <chrisw@ithemes.com>
2013-12-26 13:54:48 -0600

    Setup SSL stuff

---
+++ modules/ssl/setup.php
@@ -0,0 +1,94 @@
+<?php
+
+if ( ! class_exists( 'BWPS_SSL_Setup' ) ) {
+
+	class BWPS_SSL_Setup {
+
+		function __construct() {
+
+			global $bwps_setup_action;
+
+			if ( isset( $bwps_setup_action ) ) {
+
+				switch ( $bwps_setup_action ) {
+
+					case 'activate':
+						$this->execute_activate();
+						break;
+					case 'upgrade':
+						$this->execute_upgrade();
+						break;
+					case 'deactivate':
+						$this->execute_deactivate();
+						break;
+					case 'uninstall':
+						$this->execute_uninstall();
+						break;
+
+				}
+
+			} else {
+				wp_die( 'error' );
+			}
+
+		}
+
+		/**
+		 * Execute module activation
+		 *
+		 * @return void
+		 */
+		function execute_activate() {
+
+			$options = get_site_option( 'bwps_ssl' );
+
+			if ( $options === false ) {
+
+				$defaults = array(
+					'frontend' => 0,
+					'admin' => 0,
+					'login' => 1,
+				);
+
+				add_site_option( 'bwps_ssl', $defaults );
+
+			}
+
+		}
+
+		/**
+		 * Execute module deactivation
+		 *
+		 * @return void
+		 */
+		function execute_deactivate() {
+
+		}
+
+		/**
+		 * Execute module uninstall
+		 *
+		 * @return void
+		 */
+		function execute_uninstall() {
+
+			$this->execute_deactivate();
+
+			delete_site_option( 'bwps_ssl' );
+
+		}
+
+		/**
+		 * Execute module upgrade
+		 *
+		 * @return void
+		 */
+		function execute_upgrade() {
+
+		}
+
+	}
+
+}
+
+new BWPS_SSL_Setup();
