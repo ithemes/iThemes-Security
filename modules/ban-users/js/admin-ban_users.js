00ff8073a9d2e7227b18482107dbd35f72f8b537
Chris Wiegman <chrisw@ithemes.com>
2013-12-30 12:04:09 -0600

    Better logic for hiding the boxes.

--- modules/ban-users/js/admin-ban_users.js
+++ modules/ban-users/js/admin-ban_users.js
@@ -2,12 +2,10 @@ jQuery( document ).ready( function () {
 
 	jQuery( "#bwps_ban_users_enabled" ).change( function() {
 
-		if ( jQuery( "#bwps_ban_users_enabled" ).is( ':checked' ) || jQuery( "#setting-error-settings_updated" ).length > 0 ) {
-
+		if ( jQuery( "#bwps_ban_users_enabled" ).is( ':checked' ) || ( jQuery( "#setting-error-settings_updated" ).length > 0  && jQuery( "#setting-error-settings_updated" ).hasClass( "error" ) ) ) {
 			jQuery( "#ban_users_settings" ).show();
 
 		} else {
-
 			jQuery( "#ban_users_settings" ).hide();
 
 		}
