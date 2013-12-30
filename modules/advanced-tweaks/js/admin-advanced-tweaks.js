170f4a4dd3b93efb89900c3ea32381d0a6e40fa9
Chris Wiegman <chrisw@ithemes.com>
2013-12-30 13:29:17 -0600

    Hide settings when section not enabled.

---
+++ modules/advanced-tweaks/js/admin-advanced-tweaks.js
@@ -0,0 +1,15 @@
+jQuery( document ).ready( function () {
+
+	jQuery( "#bwps_advanced_tweaks_enabled" ).change( function() {
+
+		if ( jQuery( "#bwps_advanced_tweaks_enabled" ).is( ':checked' ) ) {
+			jQuery( "#advanced_tweaks_server, h2.settings-section-header" ).show();
+
+		} else {
+			jQuery( "#advanced_tweaks_server, h2.settings-section-header" ).hide();
+
+		}
+
+	} ).change();
+
+} );
