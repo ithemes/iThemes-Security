63790a7de6ed4ec8fad69004a6497e772c5dbcd0
Chris Wiegman <chrisw@ithemes.com>
2013-12-30 11:25:07 -0600

    finish ssl setup

--- modules/ssl/setup.php
+++ modules/ssl/setup.php
@@ -44,8 +44,22 @@ if ( ! class_exists( 'BWPS_SSL_Setup' ) ) {
 
 			if ( $options === false ) {
 
+				if ( defined( 'FORCE_SSL_LOGIN' ) && FORCE_SSL_LOGIN === true ) {
+					$login = 1;
+				} else {
+					$login = 0;
+				}
+
+				if ( defined( 'FORCE_SSL_ADMIN' ) && FORCE_SSL_ADMIN === true ) {
+					$admin = 1;
+				} else {
+					$admin = 0;
+				}
+
 				$defaults = array(
-					'frontend' => 0,
+					'frontend'	=> 0,
+					'admin'		=> $admin;
+					'login'		=> $login;
 				);
 
 				add_site_option( 'bwps_ssl', $defaults );
@@ -73,6 +87,7 @@ if ( ! class_exists( 'BWPS_SSL_Setup' ) ) {
 			$this->execute_deactivate();
 
 			delete_site_option( 'bwps_ssl' );
+			delete_metadata( 'post', null, 'bwps_enable_ssl', null, true );
 
 		}
 
