7b1000cb4b931267bc18954fcf6cd9d5e36f3997
Chris Wiegman <chrisw@ithemes.com>
2013-12-31 14:11:24 -0600

    Don’t process system tweak stuff unless the module was enabled

--- modules/advanced-tweaks/class-bwps-advanced-tweaks.php
+++ modules/advanced-tweaks/class-bwps-advanced-tweaks.php
@@ -13,69 +13,73 @@ if ( ! class_exists( 'BWPS_Advanced_Tweaks' ) ) {
 
 			$this->settings  = get_site_option( 'bwps_advanced_tweaks' );
 
-			//remove wp-generator meta tag
-			if ( isset( $this->settings['generator_tag'] ) && $this->settings['generator_tag'] == 1 ) { 
-				remove_action( 'wp_head', 'wp_generator' );
-			}
-			
-			//remove wlmanifest link if turned on
-			if ( isset( $this->settings['wlwmanifest_header'] ) && $this->settings['wlwmanifest_header'] == 1 ) {
-				remove_action( 'wp_head', 'wlwmanifest_link' );
-			}
-			
-			//remove rsd link from header if turned on
-			if ( isset( $this->settings['edituri_header'] ) && $this->settings['edituri_header'] == 1 ) {
-				remove_action( 'wp_head', 'rsd_link' );
-			}
-			
-			//ban extra-long urls if turned on
-			if ( isset( $this->settings['long_url_strings'] ) && $this->settings['long_url_strings'] == 1 && ! is_admin() ) {
-			
-				if ( 
-					! strpos( $_SERVER['REQUEST_URI'], 'infinity=scrolling&action=infinite_scroll' ) &&
-					(
-						strlen( $_SERVER['REQUEST_URI'] ) > 255 ||
-						strpos( $_SERVER['REQUEST_URI'], 'eval(' ) ||
-						strpos( $_SERVER['REQUEST_URI'], 'CONCAT' ) ||
-						strpos( $_SERVER['REQUEST_URI'], 'UNION+SELECT' ) ||
-						strpos( $_SERVER['REQUEST_URI'], 'base64' ) 
-					) 
+			if ( $this->settings['enabled'] == 1 ) { //make sure the module was enabled
 
-				) {
-					@header( 'HTTP/1.1 414 Request-URI Too Long' );
-					@header( 'Status: 414 Request-URI Too Long' );
-					@header( 'Cache-Control: no-cache, must-revalidate' );
-					@header( 'Expires: Thu, 22 Jun 1978 00:28:00 GMT' );
-					@header( 'Connection: Close' );
-					@exit;
-					
+				//remove wp-generator meta tag
+				if ( isset( $this->settings['generator_tag'] ) && $this->settings['generator_tag'] == 1 ) { 
+					remove_action( 'wp_head', 'wp_generator' );
 				}
 				
-			}
+				//remove wlmanifest link if turned on
+				if ( isset( $this->settings['wlwmanifest_header'] ) && $this->settings['wlwmanifest_header'] == 1 ) {
+					remove_action( 'wp_head', 'wlwmanifest_link' );
+				}
+				
+				//remove rsd link from header if turned on
+				if ( isset( $this->settings['edituri_header'] ) && $this->settings['edituri_header'] == 1 ) {
+					remove_action( 'wp_head', 'rsd_link' );
+				}
+				
+				//ban extra-long urls if turned on
+				if ( isset( $this->settings['long_url_strings'] ) && $this->settings['long_url_strings'] == 1 && ! is_admin() ) {
+				
+					if ( 
+						! strpos( $_SERVER['REQUEST_URI'], 'infinity=scrolling&action=infinite_scroll' ) &&
+						(
+							strlen( $_SERVER['REQUEST_URI'] ) > 255 ||
+							strpos( $_SERVER['REQUEST_URI'], 'eval(' ) ||
+							strpos( $_SERVER['REQUEST_URI'], 'CONCAT' ) ||
+							strpos( $_SERVER['REQUEST_URI'], 'UNION+SELECT' ) ||
+							strpos( $_SERVER['REQUEST_URI'], 'base64' ) 
+						) 
 
-			//display random number for wordpress version if turned on
-			if ( isset( $this->settings['random_version'] ) && $this->settings['random_version'] == 1 ) {
-				add_action( 'plugins_loaded', array( $this, 'random_version' ) );
-			}
-			
-			//remove theme update notifications if turned on
-			if ( isset( $this->settings['theme_updates'] ) && $this->settings['theme_updates'] == 1 ) {
-				add_action( 'plugins_loaded', array( $this, 'theme_updates' ) );
-			}
-			
-			//remove plugin update notifications if turned on
-			if ( isset( $this->settings['plugin_updates'] ) && $this->settings['plugin_updates'] == 1 ) {
-				add_action( 'plugins_loaded', array( $this, 'public_updates' ) );
-			}
-			
-			//remove core update notifications if turned on
-			if ( isset( $this->settings['core_updates'] ) && $this->settings['core_updates'] == 1 ) {
-				add_action( 'plugins_loaded', array( $this, 'core_updates' ) );
-			}
+					) {
+						@header( 'HTTP/1.1 414 Request-URI Too Long' );
+						@header( 'Status: 414 Request-URI Too Long' );
+						@header( 'Cache-Control: no-cache, must-revalidate' );
+						@header( 'Expires: Thu, 22 Jun 1978 00:28:00 GMT' );
+						@header( 'Connection: Close' );
+						@exit;
+						
+					}
+					
+				}
 
-			//Disable XML-RPC
-			if ( isset( $this->settings['disable_xmlrpc'] ) && $this->settings['disable_xmlrpc'] == 1 ) {
-				add_filter( 'xmlrpc_enabled', '__return_false' );
+				//display random number for wordpress version if turned on
+				if ( isset( $this->settings['random_version'] ) && $this->settings['random_version'] == 1 ) {
+					add_action( 'plugins_loaded', array( $this, 'random_version' ) );
+				}
+				
+				//remove theme update notifications if turned on
+				if ( isset( $this->settings['theme_updates'] ) && $this->settings['theme_updates'] == 1 ) {
+					add_action( 'plugins_loaded', array( $this, 'theme_updates' ) );
+				}
+				
+				//remove plugin update notifications if turned on
+				if ( isset( $this->settings['plugin_updates'] ) && $this->settings['plugin_updates'] == 1 ) {
+					add_action( 'plugins_loaded', array( $this, 'public_updates' ) );
+				}
+				
+				//remove core update notifications if turned on
+				if ( isset( $this->settings['core_updates'] ) && $this->settings['core_updates'] == 1 ) {
+					add_action( 'plugins_loaded', array( $this, 'core_updates' ) );
+				}
+
+				//Disable XML-RPC
+				if ( isset( $this->settings['disable_xmlrpc'] ) && $this->settings['disable_xmlrpc'] == 1 ) {
+					add_filter( 'xmlrpc_enabled', '__return_false' );
+				}
+
 			}
 
 		}
