adeb82dcb41c593477d1581c131cd39aa6a73e24
Chris Wiegman <chrisw@ithemes.com>
2014-01-03 10:24:27 -0600

    fixed jQuery hiding on advanced tweaks

--- modules/advanced-tweaks/js/admin-advanced-tweaks.js
+++ modules/advanced-tweaks/js/admin-advanced-tweaks.js
@@ -3,10 +3,10 @@ jQuery( document ).ready( function () {
 	jQuery( "#bwps_advanced_tweaks_enabled" ).change( function() {
 
 		if ( jQuery( "#bwps_advanced_tweaks_enabled" ).is( ':checked' ) ) {
-			jQuery( "#advanced_tweaks_server, h2.settings-section-header" ).show();
+			jQuery( "#advanced_tweaks_server, #advanced_tweaks_wordpress, h2.settings-section-header" ).show();
 
 		} else {
-			jQuery( "#advanced_tweaks_server, h2.settings-section-header" ).hide();
+			jQuery( "#advanced_tweaks_server, #advanced_tweaks_wordpress, h2.settings-section-header" ).hide();
 
 		}
 
