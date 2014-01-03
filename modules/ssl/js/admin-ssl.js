8e460a74783e5fa48d3a20bf38cc430606f8df0a
Chris Wiegman <chrisw@ithemes.com>
2014-01-03 10:37:43 -0600

    Only show alert when checking to turn ssl on

--- modules/ssl/js/admin-ssl.js
+++ modules/ssl/js/admin-ssl.js
@@ -1,3 +1,13 @@
-function forcessl() {
-	alert( ssl_warning_text.text );
-}
+jQuery( document ).ready( function () {
+
+    jQuery( '#bwps_ssl_admin, #bwps_ssl_login' ).change( function () {
+
+        if ( this.checked ) {
+
+            alert( ssl_warning_text.text );
+
+        }
+
+    } );
+
+} );
