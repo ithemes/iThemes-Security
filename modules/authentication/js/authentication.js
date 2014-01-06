407aaf60346084a1261600eb024aa9d4097b1a56
Chris Wiegman <chrisw@ithemes.com>
2014-01-06 14:41:46 -0600

    strong password enforcement now works on new user creation as well.

--- modules/authentication/js/authentication.js
+++ modules/authentication/js/authentication.js
@@ -2,7 +2,7 @@ jQuery( document ).ready( function () {
 
 	jQuery( '#resetpassform, #your-profile, #createuser' ).submit( function() {
 		
-		jQuery( '#submit' ).append( '<input type="hidden" name="password_strength" id="password_strength" value="' + jQuery( '#pass-strength-result' ).attr('class') + '">' );
+		jQuery( '#submit, #createusersub' ).append( '<input type="hidden" name="password_strength" id="password_strength" value="' + jQuery( '#pass-strength-result' ).attr('class') + '">' );
 
 	} );
 
