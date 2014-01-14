65b1c529068bccca5421a94aae7c42950fd85a8f
Chris Wiegman <chrisw@ithemes.com>
2014-01-14 15:34:48 -0600

    Lockout IP or User

--- inc/class-itsec-lockout.php
+++ inc/class-itsec-lockout.php
@@ -21,7 +21,80 @@ if ( ! class_exists( 'ITSEC_Lockout' ) ) {
 
 		public function lockout( $type, $reason, $host = null, $user = null ) {
 
-			global $wpdb;
+			global $wpdb, $itse_lib;
+
+			if ( $host !== null && ITSEC_Ban_Users_Admin::is_ip_whitelisted( sanitize_text_field( $host ) ) === false ) {
+				$good_host = $bwps_lib->validates_ip_address( $host );
+			} else {
+				$good_host = false;
+			}
+
+			if ( $user !== null ) {
+				$good_user = $bwps_lib->user_id_exists( intval( $user ) );
+			} else {
+				$good_user = false;
+			}
+
+			$blacklist_host = false;
+
+			$type = sanitize_text_field( $type );
+			$reason = sanitize_text_field( $reason );
+
+			if ( $this->settings['blacklist'] === true && $good_host === true ) { //permanent blacklist
+
+				$host_count = $wpdb->get_var( "SELECT COUNT(*) FROM `" . $wpdb->base_prefix . "itsec_lockouts` WHERE `lockout_expire` > " . date( 'Y-m-d H:i:s', $this->current_time ) . " AND lockout_host='" . esc_sql( $host ) . "';" ) + 1;
+
+				if ( $host_count >= $this->settings['blacklist_count'] ) {
+
+					ITSEC_Ban_Users_Admin::insert_ip( sanitize_text_field( $host ) );
+
+					$blacklist_host = true;
+
+				}
+
+			} 
+
+			if ( $good_host === true || $good_user === true ) {
+
+				$exp_seconds = ( intval( $this->settings( 'lockout_period' ) ) * 60 );
+				$exp = date( 'Y-m-d H:i:s', $this->current_time + $exp_seconds );
+				$exp_gmt = date( 'Y-m-d H:i:s', $this->current_time_gmt + $exp_seconds );
+
+				if ( $good_host === true && $blacklist_host === false ) { //temp lockout host
+
+					$wpdb->insert(
+						$wpdb->base_prefix . 'itsec_lockouts',
+						array(
+							'lockout_type'			=> $type,
+							'lockout_start'			=> date( 'Y-m-d H:i:s', $this->current_time ),
+							'lockout_start_gmt'		=> date( 'Y-m-d H:i:s', $this->current_time_gmt ),
+							'lockout_expire'		=> date( 'Y-m-d H:i:s', $this->current_time + $exp_seconds ),
+							'lockout_expire_gmt'	=> date( 'Y-m-d H:i:s', $this->current_time_gmt + $exp_seconds ),
+							'lockout_host'			=> sanitize_text_field( $host ),
+							'lockout_user'			=> '',
+						)
+					);
+
+				}
+
+				if ( $good_user === true ) { //blacklist host and temp lockout user
+					
+					$wpdb->insert(
+						$wpdb->base_prefix . 'itsec_lockouts',
+						array(
+							'lockout_type'			=> $type,
+							'lockout_start'			=> date( 'Y-m-d H:i:s', $this->current_time ),
+							'lockout_start_gmt'		=> date( 'Y-m-d H:i:s', $this->current_time_gmt ),
+							'lockout_expire'		=> date( 'Y-m-d H:i:s', $this->current_time + $exp_seconds ),
+							'lockout_expire_gmt'	=> date( 'Y-m-d H:i:s', $this->current_time_gmt + $exp_seconds ),
+							'lockout_host'			=> '',
+							'lockout_user'			=> intval( $user ),
+						)
+					);
+
+				} 
+
+			}
 
 		}
 
