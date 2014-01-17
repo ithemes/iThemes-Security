f48a74f3aafc6e3ae2a76050c86d44cbc6f73a90
Chris Wiegman <chrisw@ithemes.com>
2014-01-17 11:06:31 -0600

    Saving to mysql logs works

--- inc/class-itsec-logger.php
+++ inc/class-itsec-logger.php
@@ -7,15 +7,47 @@ if ( ! class_exists( 'ITSEC_Logger' ) ) {
 		private static $instance = null; //instantiated instance of this plugin
 
 		private
-			$logger_modules;
+			$logger_modules,
+			$settings;
 
 		function __construct() {
+
+			$this->settings = get_site_option( 'itsec_global' );
+
+			$this->logger_modules = array(); //array to hold information on modules using this feature
 
 			add_action( 'plugins_loaded', array( $this, 'register_modules' ) );
 
 		}
 
-		public function log_event() {
+		public function log_event( $module, $priority = 5, $data = array(), $host = '', $username = '', $user = '', $url = '', $referrer = '' ) {
+
+			global $wpdb, $itsec_current_time_gmt, $itsec_current_time;
+
+			$saved_data = array();
+
+			$options = $this->logger_modules[$module];
+
+			foreach ( $data as $key => $value ) {
+
+				$saved_data[esc_sql( $key )] = esc_sql( $value );
+			}
+
+			$wpdb->insert(
+				$wpdb->base_prefix . 'itsec_log',
+				array(
+					'log_type'     => $options['type'],
+					'log_priority' => intval( $priority ),
+					'log_date'     => date( 'Y-m-d H:i:s', $itsec_current_time ),
+					'log_date_gmt' => date( 'Y-m-d H:i:s', $itsec_current_time_gmt ),
+					'log_host'     => sanitize_text_field( $host ),
+					'log_username' => sanitize_text_field( $username ),
+					'log_user'     => intval( $user ),
+					'log_url'      => esc_sql( $url ),
+					'log_referrer' => esc_sql( $referrer ),
+					'log_data'     => serialize( $saved_data ),
+				)
+			);
 
 		}
 
