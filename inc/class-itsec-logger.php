96b5c61e2448d93baaaa6cfb9e638c9fcb354a95
Chris Wiegman <chrisw@ithemes.com>
2014-01-21 10:35:03 -0600

    Initialize a logger table class

--- inc/class-itsec-logger.php
+++ inc/class-itsec-logger.php
@@ -48,7 +48,8 @@ if ( ! class_exists( 'ITSEC_Logger' ) ) {
 			add_filter( 'itsec_add_admin_tabs', array( $this, 'add_admin_tab' ) ); //add tab to menu
 
 			if ( is_admin() ) {
-				require_once( $itsec_globals['plugin_dir'] . 'inc/lib/class-itsec-wp-list-table.php' );
+				require_once( $itsec_globals['plugin_dir'] . 'inc/logs/class-itsec-wp-list-table.php' );
+				require_once( $itsec_globals['plugin_dir'] . 'inc/logs/class-itsec-logger-tables.php' );
 			}
 
 		}
