41b2f16b6231cd714b6635b60389d746bbe97554
Chris Wiegman <chrisw@ithemes.com>
2014-01-21 12:02:54 -0600

    Displays bag login attempts log

--- modules/authentication/class-itsec-authentication-log-table.php
+++ modules/authentication/class-itsec-authentication-log-table.php
@@ -2,9 +2,9 @@
 /**
  * Log tables for Authentication Module
  *
- * @package iThemes-Security
+ * @package    iThemes-Security
  * @subpackage Authentication
- * @since 4.0
+ * @since      4.0
  */
 if ( ! class_exists( 'ITSEC_Authentication_Log_Table' ) ) {
 
@@ -14,91 +14,166 @@ if ( ! class_exists( 'ITSEC_Authentication_Log_Table' ) ) {
 
 			parent::__construct(
 				array(
-					'singular'	=> 'itsec_authentication_log_item',
-					'plural'	=> 'itsec_authentication_log_items',
-					'ajax'		=> false
+					'singular' => 'itsec_authentication_log_item',
+					'plural'   => 'itsec_authentication_log_items',
+					'ajax'     => false
 				)
 			);
 
 		}
 
-		public function display() {
+		/**
+		 * Define time column
+		 *
+		 * @param array $item array of row data
+		 * @return string formatted output
+		 *
+		 **/
+		function column_time( $item ) {
+
+			return $item['time'];
 
 		}
 
+		/**
+		 * Define host column
+		 *
+		 * @param array $item array of row data
+		 * @return string formatted output
+		 *
+		 **/
+		function column_host( $item ) {
+
+			$r = array();
+			if (!is_array($item['host'])) {
+				$item['host'] = array($item['host']);
+			}
+			foreach ($item['host'] as $host) {
+				$r[] = '<a href="http://ip-adress.com/ip_tracer/' . filter_var( $host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) . '" target="_blank">' . filter_var( $host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) . '</a>';
+			}
+			$return = implode('<br />', $r);
+			return $return;
+
+		}
+
+		/**
+		 * Define added column
+		 *
+		 * @param array $item array of row data
+		 * @return string formatted output
+		 *
+		 **/
+		function column_user( $item ) {
+
+			return $item['user'];
+
+		}
+
+		/**
+		 * Define Columns
+		 *
+		 * @return array array of column titles
+		 */
+		public function get_columns() {
+
+			return array(
+				'time' => __( 'Time', 'ithemes-security' ),
+				'host' => __( 'Host', 'ithemes-security' ),
+				'user' => __( 'Username', 'ithemes-security' ),
+			);
+
+		}
+
+		/**
+		 * Define Sortable Columns
+		 *
+		 * @return array of column titles that can be sorted
+		 */
+		public function get_sortable_columns() {
+
+			$order = ( empty( $_GET['order'] ) ) ? false : true;
+
+			$sortable_columns = array(
+				'time' => array( 'time', $order ),
+				'host' => array( 'host', $order ),
+				'user' => array( 'user', $order ),
+			);
+
+			return $sortable_columns;
+
+		}
+
+		/**
+		 * Prepare data for table
+		 *
+		 * @return void
+		 */
 		public function prepare_items() {
 
 			global $wpdb;
 
-			$columns = $this->get_columns();
-			$hidden = array();
-			$sortable = $this->get_sortable_columns();
+			$columns               = $this->get_columns();
+			$hidden                = array();
+			$sortable              = $this->get_sortable_columns();
 			$this->_column_headers = array( $columns, $hidden, $sortable );
 
-			$items = $wpdb->get_results( "SELECT * FROM `" . $wpdb->base_prefix . "itsec_log`;", ARRAY_A );
+			$items = $wpdb->get_results( "SELECT * FROM `" . $wpdb->base_prefix . "itsec_log` WHERE `log_type` = 'brute_force';", ARRAY_A );
 
-			$data = array();
+			$table_data = array();
+
+			$count = 0;
 
 			foreach ( $items as $item ) { //loop through and group 404s
 
-				if ( isset( $data[$error['url']] ) ) {
+				$table_data[$count]['time'] = $item['log_date'];
+				$table_data[$count]['host'] = $item['log_host'];
+				$table_data[$count]['user'] = $item['log_username'];
 
-					$data[$error['url']]['count'] = $data[$error['url']]['count'] + 1;
-					$data[$error['url']]['time'] = $data[$error['url']]['time'] > $error['timestamp'] ? $data[$error['url']]['last'] : $error['timestamp'];
-					$data[$error['url']]['referrer'] = $error['referrer'];
-					if ( !in_array( $error['host'], $data[$error['url']]['host'] ) ) {
-						$data[$error['url']]['host'][] = $error['host'];
-					}
-					$data[$error['url']]['id'] = $error['id'];
-					$data[$error['url']]['url'] = $error['url'];
-
-				} else {
-
-					$data[$error['url']]['count'] = 1;
-					$data[$error['url']]['time'] = $error['timestamp'];
-					$data[$error['url']]['referrer'] = $error['referrer'];
-					$data[$error['url']]['host'] = array($error['host']);
-					$data[$error['url']]['id'] = $error['id'];
-					$data[$error['url']]['url'] = $error['url'];
-					$data[$error['url']]['last'] = $error['timestamp'];
-
-				}
+				$count ++;
 
 			}
 
-			usort ( $data, array( &$this, 'sortrows' ) );
+			usort( $table_data, array( $this, 'sortrows' ) );
 
-			$per_page = 50; //50 items per page
+			$per_page     = 20; //20 items per page
 			$current_page = $this->get_pagenum();
-			$total_items = count( $data );
+			$total_items  = count( $table_data );
 
-			$data = array_slice( $data,( ( $current_page - 1 ) * $per_page ), $per_page );
+			$table_data = array_slice( $table_data, ( ( $current_page - 1 ) * $per_page ), $per_page );
 
-			$rows = array();
-			$count = 0;
+			$this->items = $table_data;
 
-			//Loop through results and take data we need
-			foreach ( $data as $item => $attr ) {
-
-				$rows[$count]['time'] = $attr['time'];
-				$rows[$count]['id'] = $attr['id'];
-				$rows[$count]['host'] = $attr['host'];
-				$rows[$count]['uri'] = $attr['url'];
-				$rows[$count]['referrer'] = $attr['referrer'];
-				$rows[$count]['count'] = $attr['count'];
-
-				$count++;
-
-			}
-
-			$this->items = $rows;
 			$this->set_pagination_args(
 				array(
 					'total_items' => $total_items,
 					'per_page'    => $per_page,
-					'total_pages' => ceil( $total_items/$per_page )
+					'total_pages' => ceil( $total_items / $per_page )
 				)
 			);
+
+		}
+
+		/**
+		 * Sorts rows by count in descending order
+		 *
+		 * @param array $a first array to compare
+		 * @param array $b second array to compare
+		 *
+		 * @return int comparison result
+		 */
+		function sortrows( $a, $b ) {
+
+			// If no sort, default to count
+			$orderby = ( !empty( $_GET['orderby'] ) ) ? esc_attr( $_GET['orderby'] ) : 'time';
+
+			// If no order, default to desc
+			$order = ( !empty( $_GET['order'] ) ) ? esc_attr( $_GET['order'] ) : 'desc';
+
+			// Determine sort order
+			$result = strcmp( $a[$orderby], $b[$orderby] );
+
+			// Send final sort direction to usort
+			return ( $order === 'asc' ) ? $result : -$result;
 
 		}
 
