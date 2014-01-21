7acc87674a9f4b4ef3344567f009bc7825ea6bb0
Chris Wiegman <chrisw@ithemes.com>
2014-01-21 13:17:25 -0600

    Fixed capitalization in class check

--- modules/authentication/index.php
+++ modules/authentication/index.php
@@ -4,7 +4,7 @@ if ( ! class_exists( 'ITSEC_Authentication' ) ) {
 	require( dirname( __FILE__ ) . '/class-itsec-authentication.php' );
 }
 
-if ( ! class_exists( 'ITSEC_Authentication_admin' ) ) {
+if ( ! class_exists( 'ITSEC_Authentication_Admin' ) ) {
 	require( dirname( __FILE__ ) . '/class-itsec-authentication-admin.php' );
 }
 
