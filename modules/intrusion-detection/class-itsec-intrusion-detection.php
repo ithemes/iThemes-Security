d9048c7394db70568e332ea18196f1f7565cb0ef
Chris Wiegman <chrisw@ithemes.com>
2014-01-22 10:46:50 -0600

    Register 404 detection with logger and lockout

--- modules/intrusion-detection/class-itsec-intrusion-detection.php
+++ modules/intrusion-detection/class-itsec-intrusion-detection.php
@@ -13,6 +13,44 @@ if ( ! class_exists( 'ITSEC_Intrusion_Detection' ) ) {
 
 			$this->settings  = get_site_option( 'itsec_intrusion-detection' );
 
+			add_filter( 'itsec_lockout_modules', array( $this, 'register_lockout' ) );
+			add_filter( 'itsec_logger_modules', array( $this, 'register_logger' ) );
+		/**
+		 * Register 404 detection for lockout
+		 *
+		 * @param  array $lockout_modules array of lockout modules
+		 *
+		 * @return array                   array of lockout modules
+		 */
+		public function register_lockout( $lockout_modules ) {
+
+			$lockout_modules['four_oh_four'] = array(
+				'type'   => 'four_oh_four',
+				'reason' => __( 'too many attempts to access a file that doesn not exist', 'ithemes-security' ),
+				'host'   => $this->settings['four_oh_four-error_threshold'],
+				'period' => $this->settings['four_oh_four-check_period']
+			);
+
+			return $lockout_modules;
+
+		}
+
+		/**
+		 * Register 404 and file change detection for logger
+		 *
+		 * @param  array $logger_modules array of logger modules
+		 *
+		 * @return array                   array of logger modules
+		 */
+		public function register_logger( $logger_modules ) {
+
+			$logger_modules['four_oh_four'] = array(
+				'type'      => 'four_oh_four',
+				'function' => __( '404 Error', 'ithemes-security' ),
+			);
+
+			return $logger_modules;
+
 		}
 
 		/**
